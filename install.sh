def2d5a042186900008ed0d75b6f125c97dee12d
iyyel <i@iyyel.io>
2020-01-21 18:06:37 +0100

    Improved repo structure.

--- install.sh
+++ install.sh
@@ -14,13 +14,18 @@
 echo "Stowing Dotfiles...";
 
 cd ~/dotfiles
+
 for file in ~/dotfiles/*; do
-  # Only run Stow on the directories in the dotfiles folder and not the individual files.
-  # Using 'basename' strips the filepath from the directory name. 
-  if [ -d ${file} ]; then
-    xstow -R $(basename $file)
-    echo "$(basename $file) stowed."; 
-  fi
+    # Only run Stow on the directories in the dotfiles folder and not the individual files.
+    # Using 'basename' strips the filepath from the directory name. 
+    if [ -d ${file} ]; then
+        if [ "$(basename $file)" == *"etc"* ]; then
+            echo "Ignored 'etc' directory."
+        else
+            xstow -R $(basename $file)
+            echo "$(basename $file) stowed."; 
+        fi
+    fi
 done
 
 # Return back to the your PWD from before you ran the script
