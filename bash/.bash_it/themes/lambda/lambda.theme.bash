e5894da9f828cb8bbc6cffed588fc447df62115b
iyyel <i@iyyel.io>
2020-06-11 21:14:53 +0200

    Added bash_it custom theme. :)

--- bash/.bash_it/themes/lambda/lambda.theme.bash
+++ bash/.bash_it/themes/lambda/lambda.theme.bash
@@ -10,7 +10,7 @@ export LS_COLORS='no=00:fi=00:di=01;34:ln=00;36:pi=40;33:so=01;35:do=01;35:bd=40
 
 function prompt_command() {
     if [ "$(whoami)" = root ]; then no_color=$red; else no_color=$white; fi
-    PS1="${blue}\W/${reset_color} $(scm_prompt_info)${blue}λ${reset_color} "
+    PS1="${blue}λ \W/${reset_color} $(scm_prompt_info)${blue}${reset_color}"
 }
 
 safe_append_prompt_command prompt_command
--- bash/.bashrc
+++ bash/.bashrc
@@ -21,7 +21,7 @@ export BASH_IT="/home/iyyel/.bash_it"
 # Lock and Load a custom theme file.
 # Leave empty to disable theming.
 # location /.bash_it/themes/
-export BASH_IT_THEME='clean'
+export BASH_IT_THEME='lambda'
 
 # (Advanced): Change this to the name of your remote repo if you
 # cloned bash-it with a remote other than origin such as `bash-it`.
