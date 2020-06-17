7e98e79dd5d03ab53eb200d08b520df71a76ac11
iyyel <i@iyyel.io>
2020-06-17 17:03:40 +0200

    Updated .files

---
+++ .extras/etc/acpi/events/lenovo-mutemic.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+INPUT_DEVICE="'Capture'"
+USER="iyyel"
+if amixer sget $INPUT_DEVICE,0 | grep '\[on\]' ; then
+    su $USER -c 'amixer -q sset Capture nocap'
+    amixer -q sset $INPUT_DEVICE nocap
+    echo "0 blink" > /proc/acpi/ibm/led
+else
+    su $USER -c 'amixer -q sset Capture cap'
+    amixer -q sset $INPUT_DEVICE cap
+    echo "0 on" > /proc/acpi/ibm/led
+fi
